e49d969efb3ff4a8742957b83c699869261683aa
MADESH KM <madeshkm@student.tce.edu>
2026-06-29 19:18:56 +0530

    Update Main.java

--- project_1/Main.java
+++ project_1/Main.java
@@ -5,14 +5,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-        
         String baseDir = new File(".").getAbsolutePath();
         tomcat.setBaseDir(baseDir);
-        
-        // Fix: Use empty string for root context
         tomcat.addWebapp("", baseDir);
-        
-        // Start server
         tomcat.start();
         System.out.println("Server started at http://localhost:8080");
         tomcat.getServer().await();
